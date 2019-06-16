e5a9e348ac92bfe46b9e7e4c3a7cc394b5d11bbe
hadaha <aungthiwin@sys-city.com>
2019-06-16 19:45:33 +0630

    adding user auth in posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :require_author, only: [:edit, :update, :destroy ]
 
   # GET /posts
   # GET /posts.json
@@ -25,6 +26,7 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
+    @post.user = current_user
 
     respond_to do |format|
       if @post.save
@@ -70,5 +72,9 @@ class PostsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
       params.fetch(:post, {}).permit(:title, :body, :photo)
+    end
+
+    def require_author
+      redirect_to(site_home_url) unless @post.user == current_user
     end
 end
