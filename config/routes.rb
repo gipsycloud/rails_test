a17439232ad2c2ac64ccca0382234cfcb073d28f
aungwin <aungthiwin@sys-city.com>
2019-06-17 16:02:47 +0630

    adding google map api 'not finished' -

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   get 'site/home'
 
   resources :posts
+  resources :places, except: [:update, :edit, :destroy]
+  resources :distances, only: [:new, :create]
+
+  root 'places#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_07_104430) do
+ActiveRecord::Schema.define(version: 2019_06_17_054240) do
+
+  create_table "places", force: :cascade do |t|
+    t.string "title"
+    t.text "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "visited_by"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
